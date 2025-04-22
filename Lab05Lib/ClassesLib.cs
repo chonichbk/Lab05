4bed2c0b95c0b40c8996bb30ac13c84081126a08
chonichbk <chonichbk@gmail.com>
2025-04-22 19:27:03 +0300

    Добавьте файлы проекта.

---
+++ Lab05Lib/ClassesLib.cs
@@ -0,0 +1,23 @@
+﻿public class Animal
+{
+    public string Name;
+    public string Species;
+    public string Color;
+    public int Age;
+    public double Weight;
+    public bool IsWild;
+    public bool CanFly;
+
+    public double AnimalYears
+    {
+        get
+        {
+            return GetAnimalYears();
+        }
+    }
+
+    public double GetAnimalYears()
+    {
+        return Age * 7.0;
+    }
+}
